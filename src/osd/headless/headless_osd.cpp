0c32fdce2358f3d8c8db858ccc5882bbf1313cc8
Sangwon Lee <gamzabaw@gmail.com>
2020-01-05 15:13:31 +0900

    Organize headless interface (#6)
    * Organize headless interface
    
    * Make mame headless instance as singleton

--- src/osd/headless/headless_osd.cpp
+++ src/osd/headless/headless_osd.cpp
@@ -8,48 +8,11 @@
 //
 //============================================================
 
-// standard includes
-
-// MAME headers
 #include "emu.h"
-
-// OSD headers
 #include "osdepend.h"
-#include "osdheadless.h"
+#include "headless_osd.h"
 
-//============================================================
-//  main
-//============================================================
-
-// int osd_entrypoint(int argc, char** argv, image_buffer_info_t *buf_info, update_callback_t cb)
-int osd_entrypoint(int argc, char** argv, image_buffer_info_t *buf_info, update_callback_t cb, void** retOSD)
-{
-	std::vector<std::string> args = osd_get_command_line(argc, argv);
-	int res = 0;
-
-	// disable I/O buffering
-	setvbuf(stdout, (char *) nullptr, _IONBF, 0);
-	setvbuf(stderr, (char *) nullptr, _IONBF, 0);
-
-	osd_options options;
-	options.set_system_name("dino");
-	options.set_value(OSD_MONITOR_PROVIDER, "headless", OPTION_PRIORITY_MAXIMUM);
-	options.set_value(OSD_KEYBOARDINPUT_PROVIDER, "headless", OPTION_PRIORITY_MAXIMUM);
-	options.set_value(OSDOPTION_SCREEN, OSDOPTVAL_AUTO, OPTION_PRIORITY_MAXIMUM);
-	options.set_value(OSDOPTION_WINDOW, 1, OPTION_PRIORITY_MAXIMUM);
-	options.set_value(OSDOPTION_RESOLUTION, "960x720@60", OPTION_PRIORITY_MAXIMUM);
-
-	headless_osd_interface osd(options);
-	osd.register_options();
-	osd.set_buffer_info(buf_info);
-	osd.set_update_callback(cb);
-
-	*retOSD = (void *)&osd;
-
-	res = emulator_info::start_frontend(options, osd, args);
-
-	exit(res);
-}
+void on_mame_machine_setup();
 
 //============================================================
 //  constructor
@@ -98,4 +61,9 @@ void headless_osd_interface::init(running_machine &machine)
 	osd_common_t::init(machine);
 
 	osd_common_t::init_subsystems();
+}
+
+void headless_osd_interface::on_machine_setup()
+{
+	on_mame_machine_setup();
 }
